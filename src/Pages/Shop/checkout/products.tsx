0979dd9176c05a2bdd16cfec3f32e3660e62cb36
qraxiss <qraxiss@gmail.com>
2024-03-25 19:31:16 +0300

    checkout

--- src/Pages/Shop/checkout/products.tsx
+++ src/Pages/Shop/checkout/products.tsx
@@ -1,88 +1,109 @@
 // import { useCart } from 'oldcontext/cart'
+import { CartProvider, useCart } from 'context/cart'
+import { useEffect, useState } from 'react'
 import { Table, Image, Card } from 'react-bootstrap'
 
+function Items({ items }: { items: any[] }) {
+    return items.map((item: any, inx: number) => {
+        return (
+            <tr key={inx}>
+                <td className="justify-center-center">
+                    <div className="d-flex align-items-center gap-2">
+                        <Image src={item.variant.product.image} alt="" className="cart-img" />
+                        <h6>
+                            {item.variant.product.name} x {item.count}
+                        </h6>
+                    </div>
+                </td>
+                <td className="justify-center-center">
+                    <div className="d-flex align-items-center gap-2">
+                        <Image src={item.variant.product.image} alt="" className="invisible" />
+                        <h6>${(item.variant.price * item.count).toFixed(2)}</h6>
+                    </div>
+                </td>
+            </tr>
+        )
+    })
+}
+
 export function Products() {
-    // let { cartItems } = useCart()
 
-    let price = 0
 
-    // cartItems.forEach((item) => {
-    //     price += item.count * item.product.price
-    // })
+    function Compnent() {
+        const { cartGQL } = useCart()
+        
+        const [items, setItems] = useState<any>()
+
+        useEffect(()=>{
+            if (cartGQL){
+                switch (cartGQL?.status){
+                    case 'success':{
+                        setItems(<Items items={cartGQL!.data!.items}/>)
+                    }
+                }
+            }
+        }, [cartGQL?.status])
+
+        let price = 0
+        return (
+            <Card>
+                <Card.Body>
+                    <div className="table-responsive table-card">
+                        <Table className="table-borderless table-nowrap mb-0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Product</th>
+                                    <th scope="col">Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>{items}</tbody>
+                        </Table>
+
+                        <Table className="table-borderless mb-0 fs-15">
+                            <tbody>
+                                <tr className="table-active">
+                                    <th>Shipping</th>
+                                    <td className="text-end">
+                                        <span className="fw-semibold cart-total">${((price / 100) * 5).toFixed(2)}</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+
+                        <Table className="table-borderless mb-0 fs-15">
+                            <tbody>
+                                <tr className="table-active">
+                                    <th>Subtotal</th>
+                                    <td className="text-end">
+                                        <span className="fw-semibold cart-total">${price.toFixed(2)}</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+
+                        <hr />
+
+                        <Table className="table-borderless mb-0 fs-15">
+                            <tbody>
+                                <tr className="table-active">
+                                    <th>
+                                        <h6>Total</h6>
+                                    </th>
+                                    <td className="text-end">
+                                        <span className="fw-semibold cart-total">${(price + (price / 100) * 5).toFixed(2)}</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </div>
+                </Card.Body>
+            </Card>
+        )
+    }
 
     return (
-        <Card>
-            <Card.Body>
-                <div className="table-responsive table-card">
-                    <Table className="table-borderless table-nowrap mb-0">
-                        <thead>
-                            <tr>
-                                <th scope="col">Product</th>
-                                <th scope="col">Subtotal</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* {cartItems.map((item: any, inx: number) => {
-                                return (
-                                    <tr key={inx}>
-                                        <td className="justify-center-center">
-                                            <div className="d-flex align-items-center gap-2">
-                                                <Image src={item.product.image} alt="" className="cart-img" />
-                                                <h6>
-                                                    {item.product.name} x {item.count}
-                                                </h6>
-                                            </div>
-                                        </td>
-                                        <td className="justify-center-center">
-                                            <div className="d-flex align-items-center gap-2">
-                                                <Image src={item.product.image} alt="" className="invisible" />
-                                                <h6>${(item.product.price * item.count).toFixed(2)}</h6>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )
-                            })} */}
-                        </tbody>
-                    </Table>
-
-                    <Table className="table-borderless mb-0 fs-15">
-                        <tbody>
-                            <tr className="table-active">
-                                <th>Shipping</th>
-                                <td className="text-end">
-                                    <span className="fw-semibold cart-total">${((price / 100) * 5).toFixed(2)}</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-
-                    <Table className="table-borderless mb-0 fs-15">
-                        <tbody>
-                            <tr className="table-active">
-                                <th>Subtotal</th>
-                                <td className="text-end">
-                                    <span className="fw-semibold cart-total">${price.toFixed(2)}</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-
-                    <hr />
-
-                    <Table className="table-borderless mb-0 fs-15">
-                        <tbody>
-                            <tr className="table-active">
-                                <th>
-                                    <h6>Total</h6>
-                                </th>
-                                <td className="text-end">
-                                    <span className="fw-semibold cart-total">${(price + (price / 100) * 5).toFixed(2)}</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </Card.Body>
-        </Card>
+        <CartProvider>
+            <Compnent />
+        </CartProvider>
     )
 }
