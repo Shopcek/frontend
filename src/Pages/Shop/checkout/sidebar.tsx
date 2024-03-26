b26fa071735a8ac7644460255dbc5eae25af38a5
qraxiss <qraxiss@gmail.com>
2024-03-26 09:56:33 +0300

    prices added to checkout

--- src/Pages/Shop/checkout/sidebar.tsx
+++ src/Pages/Shop/checkout/sidebar.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, Form, Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
-import { useBinance } from 'context/binance'
-// import { useCart } from 'oldcontext/cart'
+import { useBinance, BinanceProvider } from 'context/binance'
+import { useCart, CartProvider } from 'context/cart'
 import { buyWithWallet } from 'lib/rainbow'
 // import { useOrder } from 'oldcontext/order'
 
@@ -20,77 +20,114 @@ export function order({ newOrder }: { newOrder: any }) {
 }
 
 export const Shoporder = () => {
-    let navigate = useNavigate()
+    function Component() {
+        let navigate = useNavigate()
 
-    // let { bnb } = useBinance()
-    // let { cartItems } = useCart()
-    // let { newOrderGQL, newOrder } = useOrder()
+        let { bnb } = useBinance()
+        let { cartGQL, cartId } = useCart()
+        useEffect(() => {
+            cartGQL?.fn({
+                variables: {
+                    id: cartId
+                }
+            })
+        }, [])
 
-    // useEffect(() => {
-    //     if (newOrder && !newOrderGQL.loading) {
-    //         navigate(`/shop/success/${newOrder.id}`)
-    //     }
-    // }, [newOrderGQL.loading])
+        console.log(bnb)
 
-    // let orderfn = order({ newOrder: newOrderGQL })
+        const [pay, setPay] = useState<any>()
 
-    // let total: number = 0
-    // cartItems.forEach((item) => {
-    //     total = total + item.count * item.product.price
-    // })
+        useEffect(() => {
+            if (cartGQL) {
+                switch (cartGQL.status) {
+                    case 'success': {
+                        const price = cartGQL!.data!.price / bnb
 
-    // let price = (total / bnb) * (103 / 100)
+                        if (bnb===0){
+                            break
+                        }
+
+                        setPay(
+                            <Button
+                                className="btn btn-hover btn-soft-info info-text"
+                                onClick={() => {
+                                    buyWithWallet(console.log, price)
+                                }}
+                            >
+                                Pay {price.toFixed(3)} BNB <i className="ri-arrow-right-line label-icon align-middle ms-1"></i>
+                            </Button>
+                        )
+                        break
+                    }
+
+                    default: {
+                        setPay(
+                            <Button
+                                className="btn btn-hover btn-soft-info info-text"
+                                onClick={() => {
+                                    buyWithWallet(console.log, cartGQL!.data!.price)
+                                }}
+                                disabled
+                            >
+                                Price calculating... <i className="ri-arrow-right-line label-icon align-middle ms-1"></i>
+                            </Button>
+                        )
+                    }
+                }
+            }
+        }, [cartGQL?.status, bnb])
+
+        return (
+            <React.Fragment>
+                <Card>
+                    <Card.Body>
+                        <div className="text-center">
+                            <h6 className="mb-3 fs-15">
+                                Have a <span className="fw-semibold">promo</span> code ?
+                            </h6>
+                        </div>
+                        <div className="hstack gap-3 px-3 mx-n3">
+                            <Form.Control
+                                className="me-auto"
+                                type="text"
+                                placeholder="Enter coupon code"
+                                defaultValue="Shopcek"
+                                aria-label="Add Promo Code here..."
+                            />
+                            <button type="button" className="btn btn-primary w-xs">
+                                Apply
+                            </button>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className="overflow-hidden">
+                    <Card.Header className="border-bottom-dashed">
+                        <h5 className="card-title mb-0 fs-15">Payment Information</h5>
+                    </Card.Header>
+                    <Card.Body className=" pt-4">
+                        <div className="table-responsive table-card justify-content-center">
+                            <div className="info-text">
+                                <ConnectButton />
+                            </div>
+
+                            <div className="info-text">
+                                Your personal data will be used to process your order, support your experience throughout this website, and for other
+                                purposes described in our privacy policy.
+                            </div>
+                            <Form.Check className="info-text" label="I have read and agree to the website terms and conditions" />
+                        </div>
+                    </Card.Body>
+                    {pay}
+                </Card>
+            </React.Fragment>
+        )
+    }
 
     return (
-        <React.Fragment>
-            <Card>
-                <Card.Body>
-                    <div className="text-center">
-                        <h6 className="mb-3 fs-15">
-                            Have a <span className="fw-semibold">promo</span> code ?
-                        </h6>
-                    </div>
-                    <div className="hstack gap-3 px-3 mx-n3">
-                        <Form.Control
-                            className="me-auto"
-                            type="text"
-                            placeholder="Enter coupon code"
-                            defaultValue="Shopcek"
-                            aria-label="Add Promo Code here..."
-                        />
-                        <button type="button" className="btn btn-primary w-xs">
-                            Apply
-                        </button>
-                    </div>
-                </Card.Body>
-            </Card>
-            <Card className="overflow-hidden">
-                <Card.Header className="border-bottom-dashed">
-                    <h5 className="card-title mb-0 fs-15">Payment Information</h5>
-                </Card.Header>
-                <Card.Body className=" pt-4">
-                    <div className="table-responsive table-card justify-content-center">
-                        <div className="info-text">
-                            <ConnectButton />
-                        </div>
-
-                        <div className="info-text">
-                            Your personal data will be used to process your order, support your experience throughout this website, and for other
-                            purposes described in our privacy policy.
-                        </div>
-                        <Form.Check className="info-text" label="I have read and agree to the website terms and conditions" />
-                    </div>
-                </Card.Body>
-
-                <Button
-                    className="btn btn-hover btn-soft-info info-text"
-                    onClick={() => {
-                        // buyWithWallet(orderfn, price)
-                    }}
-                >
-                    Pay {{price:0}.price.toFixed(3)} BNB <i className="ri-arrow-right-line label-icon align-middle ms-1"></i>
-                </Button>
-            </Card>
-        </React.Fragment>
+        <CartProvider>
+            <BinanceProvider>
+                <Component />
+            </BinanceProvider>
+        </CartProvider>
     )
 }
